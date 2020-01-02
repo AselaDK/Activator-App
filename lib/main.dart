4f2a486ff3164f9baf153a19ddb0206761cfe579
PAMALSAHAN <p.s.jayawickrama@gmail.com>
2020-01-02 22:02:27 +0530

    conflict resolved

--- lib/main.dart
+++ lib/main.dart
@@ -1,56 +1,4 @@
-//import 'package:flutter/material.dart';
-//
-//void main() => runApp(MyApp());
-//
-//class MyApp extends StatelessWidget {
-//
-//  @override
-//  Widget build(BuildContext context) {
-//    return MaterialApp(
-//      debugShowCheckedModeBanner: false,
-//      title: 'ACTIVATOR: READERS',
-//      theme: ThemeData(
-//        primarySwatch: Colors.blue,
-//      ),
-//      home: MyHomePage(title: 'ACTIVATOR: READERS'),
-//    );
-//  }
-//}
-//
-//class MyHomePage extends StatefulWidget {
-//  MyHomePage({Key key, this.title}) : super(key: key);
-//
-//  final String title;
-//
-//  @override
-//  _MyHomePageState createState() => _MyHomePageState();
-//}
-//
-//class _MyHomePageState extends State<MyHomePage> {
-//
-//  @override
-//  Widget build(BuildContext context) {
-//
-//    return Scaffold(
-//      appBar: AppBar(
-//        title: Text(widget.title),
-//      ),
-//      body: Center(
-//        child: Column(
-//          mainAxisAlignment: MainAxisAlignment.center,
-//          children: <Widget>[
-<<<<<<< HEAD
-//            Text("I'm a Reader"),
-=======
-//            Text("I'm a Reader from pamal pamal"),
->>>>>>> 0e003a736046db963284dab25cc226ebc5130665
-//          ],
-//        ),
-//      ),
-//    );
-//  }
-//}
-<<<<<<< HEAD
+
 
 import 'package:flutter/material.dart';
 void main(){
@@ -60,17 +8,23 @@ void main(){
   ));  // use material app
 }
 
-class HomeScreen extends StatelessWidget{
+class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
 
+     appBar: AppBar(
+       backgroundColor: Colors.red,
+       title: Text("this is app title"),
+
+     ),
       body: Container(
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height,
         color: Colors.blue,
         child: Center(
+
           child: Container(
             width: 300,
             height: 300,
@@ -79,19 +33,22 @@ class HomeScreen extends StatelessWidget{
 
               children: <Widget>[
 
+
                 Material(
 
-                  elevation: 100.0,
-                   borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                   color: Colors.deepOrange,
+                 elevation: 5.0,
+                  borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                  color: Colors.deepOrange,
+
                   child :TextField(
+
                       style: TextStyle(
                           fontSize: 20.0,
-                          color: Colors.orange
+                          color: Colors.black
                       )
-
-
                   ),
+
+
                 )
 
 
@@ -102,34 +59,8 @@ class HomeScreen extends StatelessWidget{
       ),
     );
   }
-=======
-
-// create login page
-
-//import 'material';
-import 'package:flutter/material.dart';
-
-void main(){
-  runApp(MaterialApp(
-    title: "login app",
-    home: HomeScreen(),
-  ));
 }
 
-class HomeScreen extends StatelessWidget{
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return Scaffold(
-     
-      body: Container(
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
 
-        color: Colors.lightGreenAccent,
-      ),
-    );
-  }
 
->>>>>>> 0e003a736046db963284dab25cc226ebc5130665
-}
+
